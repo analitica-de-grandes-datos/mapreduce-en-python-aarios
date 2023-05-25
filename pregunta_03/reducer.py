d58ee38b1974b3ed32f391dd52f9d7ac1fdcee6a
aarios <aarios@unal.edu.co>
2023-05-24 20:06:22 -0500

    revision punto 3

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys 
+
+if __name__ == '__main__':
+    l = []
+    for line in sys.stdin:
+        k, v = line.split(',')
+        l.append((k,v))
+    l.sort(key = lambda x:x[1], reverse = True)
+    for i in l:
+        sys.stdout.write("{},{}\n").format(i[0], i[1])
+
