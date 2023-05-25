eb1834e5766216d7f2cb6ff7f8f8c126ab3102b2
aarios <aarios@unal.edu.co>
2023-05-24 19:30:34 -0500

    Revision P9 - MR

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys 
+
+if _name_ == "_main_":
+    for line in sys.stdin:
+        clave, letras = line.strip().split("\t")
+        for letra in letras.split(','):
+            sys.stdout.write("{} {}\n".format(clave, letra))
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys 
+
+if _name_ == '_main_':
+    l = []
+    for line in sys.stdin:
+        key, fe, val = line.strip().split(",")
+        l.append((key, fe, val))
+    l = sorted(l, key=lambda x: int(x[2]))[:6]
+    for i in l:
+        sys.stdout.write("{}   {}   {}\n".format(i[0], i[1], i[2]))
