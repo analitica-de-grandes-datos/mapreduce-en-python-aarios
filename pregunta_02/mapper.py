d647cee19dd0c5a1ab272790c658afefd46c8634
aarios <aarios@unal.edu.co>
2023-05-30 21:43:17 -0500

    correccion del punto 2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -2,9 +2,8 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     for line in sys.stdin:
         line = line.strip()
-        line = line.split("   ")
-        sys.stdout.write("{}\n".format(line[0]))
+        splits = line.split(',')
+        sys.stdout.write("{}\t{}\n".format(splits[3], splits[4]))
